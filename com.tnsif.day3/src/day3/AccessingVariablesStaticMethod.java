810e95a47adbfec953ec6edaf186d054e591dfcd
SaiKrishna7211 <168553786+SaiKrishna7211@users.noreply.github.com>
2024-05-25 10:47:15 +0530

    Add files via upload

---
+++ com.tnsif.day3/src/day3/AccessingVariablesStaticMethod.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class AccessingVariablesStaticMethod {
+	int a=10;
+	static int b=20;
+	static void display() {
+		System.out.println(AccessingVariablesStaticMethod.b);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessingVariablesStaticMethod.display();
+	}
+
+}
+
